5818249f2989c6b83af3a10948687cab8625a76b
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-02-16 23:53:08 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:susya/main.dart';
+import 'package:unisys/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
