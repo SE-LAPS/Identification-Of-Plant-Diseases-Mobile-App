3328fcca67c0a1136a59bad04ddad3994679ec82
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-02-19 16:56:39 +0530

    Update main.cpp

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"afet_yonetim", origin, size)) {
+  Win32Window::Size size(600, 800);
+  if (!window.Create(L"Plants Doctor", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
