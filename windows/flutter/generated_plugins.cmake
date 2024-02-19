9b11f1e39e33b060fd81c39b7511d0c52f92c2b2
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-02-19 16:56:42 +0530

    Update generated_plugins.cmake

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,10 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
+  firebase_auth
   firebase_core
-  geolocator_windows
-  objectbox_flutter_libs
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
